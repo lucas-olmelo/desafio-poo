fc0c0f889cf625f73b66bd45cb9e9f1619c13360
lucas-olmelo <lucaas1904@gmail.com>
2022-11-29 22:13:56 -0300

    Classes Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import melo.desafio.dominio.Curso;
+import melo.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso();
+
+        curso.setTitulo("Curso de Java");
+        curso.setDescricao("Curso de Java muito legal e interessante");
+        curso.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso de Python");
+        curso2.setDescricao("Curso de Python muito legal e interessante");
+        curso2.setCargaHoraria(12);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria muito legal de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
